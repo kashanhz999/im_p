0e1130d217a8625e04066f79c18e9b08c5bffbb8
Kashan Haider <19cs162@jssaten.ac.in>
2024-11-12 12:47:27 +0530

    build Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import About from "@/components/about";
-import Contact from "@/components/contact";
+// import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Footer from "@/components/footer";
 import Intro from "@/components/intro";
